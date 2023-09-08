36b2b175460e839fbfa802d458f8865cbc501a59
Fineas <r0845664@student.thomasmore.be>
2023-09-08 19:42:59 +0200

    Project aangemaakt en de input validatie geprogrammeerd

---
+++ palindrome_checker/palindrome_checker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace palindrome_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Geef een woord in: ");
+                input = Console.ReadLine();
+            } while(string.IsNullOrWhiteSpace(input));
+
+            string res=input.Split().Reverse().ToString();
+            Console.WriteLine(res);
+        }
+    }
+}
